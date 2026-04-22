be7c7faf7ead78836ff89bebf0055b366cb3b768
Erjoon7 <erjonhamidi04@gmail.com>
2026-04-22 16:35:13 +0200

    add PUT /projects endpoint

--- backend/project-service/src/routes/project.routes.js
+++ backend/project-service/src/routes/project.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getProjects, createProject } = require('../controllers/project.controller');
+const { getProjects, createProject, updateProject } = require('../controllers/project.controller');
 
 router.get('/', getProjects);
 router.post('/', createProject);
+router.put('/:id', updateProject);
 
 module.exports = router;
