78cb1256b905758f71853bc09889ce3f194f1347
Erjoon7 <erjonhamidi04@gmail.com>
2026-05-01 23:51:33 +0200

    chore(project): update routes and dependencies

--- backend/project-service/src/routes/project.routes.js
+++ backend/project-service/src/routes/project.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getProjects, createProject, updateProject, deleteProject } = require('../controllers/project.controller');
+const verifyToken = require('../middleware/verifyToken');
 
-router.get('/', getProjects);
-router.post('/', createProject);
-router.put('/:id', updateProject);
-router.delete('/:id', deleteProject);
+router.get('/', verifyToken, getProjects);
+router.post('/', verifyToken, createProject);
+router.put('/:id', verifyToken, updateProject);
+router.delete('/:id', verifyToken, deleteProject);
 
 module.exports = router;
